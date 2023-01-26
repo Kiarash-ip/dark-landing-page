d8ff928087413f12fc4435e645f3a008ad33c26d
Kiarash Iranpour <kia.iranpour1380@gmail.com>
2023-01-26 16:59:43 +0330

    new character added and backgrounds modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,10 @@ import React, { useRef, useEffect, useState } from "react";
 import "./Header.scss";
 import { useScroll, animated, useSpring } from "@react-spring/web";
 
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
 export default function Header() {
   const containerRef = useRef(null);
   const [scrollTop, setScrollTop] = useState(0);
@@ -23,33 +27,60 @@ export default function Header() {
   });
 
   useEffect(() => {
-    console.log(scrollYProgress);
+    console.log(scrollTop);
   });
 
   return (
     <div ref={containerRef} className="header">
-      <div className="animated__layers">
+      <animated.div
+        className="animated__layers"
+        style={{
+          transform: `translateY(${
+            scrollTop > 0.8 ? scale(scrollTop, 0.8, 1, 0, -200) : 0
+          }%)`,
+        }}
+      >
         <img
-          src="/public/images/BG Hero.png"
+          src="/public/images/minecraft_nether.png"
           className="header__top--gradient"
-        />
-        <img src="/public/images/HG.png" className="header__sky__img" />
-        <img src="/public/images/MG.png" className="header__mountains__img" />
-        <animated.div
           style={{
-            right: scrollTop * 1000,
-            position: "absolute",
-            top: "50%",
+            transform: `translateX(${
+              scrollTop < 0.8 ? scale(scrollTop, 0, 0.8, 200, 0) : 0
+            }%)`,
+          }}
+        />
+        <img
+          src="/public/images/minecraft_normal.png"
+          className="header__sky__img"
+          style={{
+            transform: `translateX(${scale(scrollTop, 0, 0.8, 100, -100)}%)`,
+          }}
+        />
+        <img
+          src="/public/images/minecraft_snow.png"
+          className="header__mountains__img"
+          style={{
+            transform: `translateX(${scale(scrollTop, 0, 0.4, 0, -100)}%)`,
+          }}
+        />
+        <animated.div
+          className="animated__ghost"
+          style={{
+            // right: scrollTop * 1000,
+            left: `${scale(scrollTop, 0, 1, 0, 100)}%`,
           }}
         >
-          <img src="/public/images/VG.png" className="header__person__img" />
+          <img
+            src="/public/images/minecraft-peq.gif"
+            className="header__person__img"
+          />
         </animated.div>
-        <img
+        {/* <img
           src="/public/images/BG Content.png"
           className="header__bottom--gradient"
-        />
-      </div>
-      {new Array(5).fill(null).map((_, index) => (
+        /> */}
+      </animated.div>
+      {new Array(10).fill(null).map((_, index) => (
         <div className="full__page" key={index} />
       ))}
     </div>
